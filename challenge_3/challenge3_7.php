49ca0737583517a91393ca265948f2c908fc1042
Yoichiro Shimizu <yoichi.simizu@gmail.com>
2015-12-24 09:52:35 +0900

    途中までupさせて頂きます。

---
+++ challenge_3/challenge3_7.php
@@ -0,0 +1,16 @@
+﻿<?php
+  $global_num = 3;
+  function count_num(){
+    static $number = 0;
+    global $global_num;
+    $number += 1;
+    echo "Function call:$number<br>";
+    echo "Result:$global_num<br>";
+    $global_num *= 2;
+  }
+
+  for ($i=0;$i<20;$i++) {
+    count_num();
+  }
+
+?>
---
+++ challenge_3/util.php
@@ -0,0 +1,16 @@
+<?php
+  function my_intro(){
+    echo ("name:清水 要一郎.<br>");
+    echo ("birthday:1991/04/01.<br>");
+    echo ("introduction:趣味は酒.<br>");
+  }
+
+  function decision($num){
+    if ($num % 2 == 0) {
+      print "偶数";
+    }else{
+      print "奇数";
+    }
+  }
+
+ ?>
