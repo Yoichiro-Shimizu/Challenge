52a76f2b8916759b1b7b578552e68845ca43eb36
Yoichiro Shimizu <yoichi.simizu@gmail.com>
2015-12-25 10:49:15 +0900

    3_5を訂正させて頂きました。4_2も問題の解釈が全く異なっていたため同様です。

--- challenge_3/challenge3_5.php
+++ challenge_3/challenge3_5.php
@@ -3,14 +3,14 @@
     $id = 0;
     $name = "Spam";
     $birthday = "1990/12/31";
-    $address = "Heaven";
+    $address = "0";
     $arrayprof = array($id, $name, $birthday, $address);
     return $arrayprof;
   }
   $results = his_profile();
 
   foreach ($results as $key => $value){
-    if (0 == strcmp($value, 0)){
+    if (0 == strcmp($value, 0) && is_int($value)){//$idを整数と仮定する
       continue;
     }
     echo ("$value<br>");
